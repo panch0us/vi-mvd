507ebdbc4e555099921b08160da7dc951a5cdbdf
panch0us <godbryansk@yandex.ru>
2022-11-29 21:48:20 +0300

    test sintax

--- vi-mvd/course1_control1_task5-2.cpp
+++ vi-mvd/course1_control1_task5-2.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
+#include <iomanip>
+
 
 int main() {
+	int a = 0;
+	float b = 5;
 
+	double i = 12345.1234567;
+	std::cout << std::fixed << std::setprecision(5) << i << std::endl;
+	std::cout << std::setw(20) << std::showpoint << std::setprecision(2) << b << "QWE";
 }
