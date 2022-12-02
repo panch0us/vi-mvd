6daece9ab54f8c3f8e65d4039cb7c6f2003fa57e
panch0us <godbryansk@yandex.ru>
2022-12-02 20:06:33 +0300

    solving 2 task

--- vi-mvd/course1_control1_task5-2.cpp
+++ vi-mvd/course1_control1_task5-2.cpp
@@ -1,10 +1,22 @@
+/*
+«Дана целочисленная матрица A размером mxn 
+упорядочить (переставить) столбцы матрицы по 
+неубыванию (каждый слежующий элемент не меньше 
+предыдущего) значений первых элементов столбцов.
+Разрешается использовать дополнительный массив».
+*/
+
+
 #include <iostream>
+#include <Windows.h>
 using namespace std;
 
-int** arr_f(int &rows, int &cols) {
+int** arr_f(int &rows, int &cols, int &random) {
 
-	cout << "count rows: "; cin >> rows;
-	cout << "count cols: "; cin >> cols;
+	
+	cout << "Введите количество строк матрицы: "; cin >> rows;
+	cout << "Введите количество столбцов матрицы: "; cin >> cols;
+	cout << "Введите диапазон для случайных чисел: "; cin >> random;
 
 	int** arr = new int *[rows];
 
@@ -17,7 +29,7 @@ int** arr_f(int &rows, int &cols) {
 	{
 		for (int j = 0; j < cols; j++)
 		{
-			*(*(arr + i) + j) = rand() % 10;
+			*(*(arr + i) + j) = rand() % random;
 		}
 	}
 
@@ -25,11 +37,16 @@ int** arr_f(int &rows, int &cols) {
 }
 
 int main() {
-	int rows = 0, cols = 0, summ = 0, count = 0, average = 0;
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
 
-	int **array = arr_f(rows, cols);
+	int rows = 0, cols = 0, buffer_1 = 0, buffer_2 = 0, count = 0, random = 0;
 
-	cout << "ARRAY: " << endl;
+	int **array = arr_f(rows, cols, random);
+
+	cout << endl << "Матрица " << rows << "x" 
+		<< cols << ", состоящая из случайных чисел в диапазоне от 0 до " 
+		<< random << ": " << endl;
 
 	for (int i = 0; i < rows; i++)
 	{
@@ -40,24 +57,34 @@ int main() {
 		cout << endl;
 	}
 
+	for (int j = 0; j < cols; j++)
+	{
+		for (int i = 0; i < rows; i++)
+		{
+			for (int j = 0; j < cols - 1; j++)
+			{
+				if (array[i][j] > array[i][j + 1]) {
+					buffer_1 = array[i][j];
+					buffer_2 = array[i][j + 1];
+					array[i][j] = buffer_2;
+					array[i][j + 1] = buffer_1;
+				}
+			}
+		}
+	}
+		
+	
+	cout << endl << "Матрица с сортировкой по неубыванию:" << endl;
 	for (int i = 0; i < rows; i++)
 	{
 		for (int j = 0; j < cols; j++)
 		{
-			if ( !(*(*(array + i) + j) % 2) ) {
-				summ += *(*(array + i) + j);
-				count += 1;
-			}
+			cout << *(*(array + i) + j) << '\t';
 		}
+		cout << endl;
 	}
 
-	if (count) {
-		average = summ / count;
-	}
-
-	cout << "summ = " << summ << endl << "count = " << count << endl << "average = " << average << endl;
-
-
+	// освобождаем память
 	for (int i = 0; i < cols; i++)
 	{
 		delete[] array[i];
