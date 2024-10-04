3db2f89daa5055bdfc017b6233198ffc98daa5f2
panch0us <godbryansk@yandex.ru>
2024-10-04 18:22:41 +0300

    added search by person

--- vi-mvd-third-course/main.c
+++ vi-mvd-third-course/main.c
@@ -16,23 +16,27 @@
     "===============================================================\n"
 
 
-/* Определяем константы */
-enum {  max_name_len = 64 };
+/* Create constans */
+enum {  MAX_NAME_LEN = 64 };
 
 
-/* Определяем струкрутру лица для опознания*/
+/* Create struct for insert/select in database. 
+Person.sex = 3 char, because russian char = 2 byte + 1 byte for \0
+*/
 struct person {
-    char surname[max_name_len];             // Фамилия
-    char name[max_name_len];                // Имя
-    char middle_name[max_name_len];         // Отчество
-    char sex[3];                            // М или Ж
+    char surname[MAX_NAME_LEN];
+    char name[MAX_NAME_LEN];
+    char middle_name[MAX_NAME_LEN];
+    char sex[3];                        /* М or Ж */
 };
 
+int callback(void*, int, char**, char**);
 
 int main()
 {
-    sqlite3 *db;                                        /* Создаем указатель на базу данных */
-    int result = sqlite3_open("idetification.db", &db); /* Открываем подключение в БД */
+    /* Create pointer on  database */
+    sqlite3 *db;
+    int result = sqlite3_open("idetification.db", &db);
     
     if(result != SQLITE_OK){
         fprintf(stderr, "Error: %s\n", sqlite3_errmsg(db));
@@ -40,6 +44,7 @@ int main()
         return 1;
     }
     
+    /* Attempt create table */
     char *sql = "CREATE TABLE IF NOT EXISTS person(\
                  id INTEGER PRIMARY KEY AUTOINCREMENT,\
                  surname TEXT,\
@@ -47,7 +52,7 @@ int main()
                  middle_name TEXT,\
                  sex TEXT\
                  )";
-
+    
     char *err_msg = 0;
     result = sqlite3_exec(db, sql, 0, 0, &err_msg);
 
@@ -72,10 +77,10 @@ int main()
     while((select_menu = getchar()) != EOF) {
         switch(select_menu)  
         {
+            /* create person */
             case '1':
                 getchar(); 
             
-                // Блок ввода фамилии
                 printf("Вы выбрали ввод лица!\nВведите фамилию: ");
                 scanf_result = scanf("%63[^'\n']", prsn.surname);
                 if(scanf_result != 1){
@@ -83,100 +88,185 @@ int main()
                     return 1;
                 
                 }
-                 getchar();
+                getchar();
             
                 printf("Введите имя: ");
                 scanf_result = scanf("%63[^'\n']", prsn.name);
-            if(scanf_result != 1){
-                printf("Error: wrong input.\n");
-                return 1;
-            }
-            getchar();
-            
-            printf("Введите отчество: ");
-            scanf_result = scanf("%63[^'\n']", prsn.middle_name);
-            if(scanf_result != 1){
-                printf("Error: wrong input.\n");
-                return 1;
-            }
-            getchar();
-
-            // Блок ввода пола
-            printf("Введите пол: ");
-            scanf_result = scanf("%2[^'\n']", prsn.sex);
-            if(scanf_result != 1){
-                printf("Error: wrong input.\n");
-                return 1;
-            }
-            getchar();
-            
-            printf("-------------------------------------------------\n \
-                \rВнимание! Проверьте введенные данные!\n \
-                \rВы ввели следующие данные:\n \
-                \rФамилия: %s\n \
-                \rИмя: %s\n \
-                \rОтчество: %s\n \
-                \rПол: %s\n \
-                \r--------------------------------------------------- \n \
-                \r           1. Верно\n \
-                \r           2. Ввести заново\n \
-                \r---------------------------------------------------\n\n",
-                prsn.surname, prsn.name, prsn.middle_name, prsn.sex); 
-            
-            while((select_menu = getchar()) != EOF) {
-                switch(select_menu){
-                    case '1':
-                       getchar();
-                       printf("aaaa\n");
-                       break;
-
-                    default:
-                       getchar();
-                       printf("default\n");
-                       break;
+                if(scanf_result != 1){
+                    printf("Error: wrong input.\n");
+                    return 1;
                 }
-                break;
-            }
+                getchar();
+                
+                printf("Введите отчество: ");
+                scanf_result = scanf("%63[^'\n']", prsn.middle_name);
+                if(scanf_result == '\n'){
+                    getchar();
+                    scanf_result = (int)32; // Если отчества нет - символ пробела ASCII
+                    return 1;
+                }
+                getchar();
 
-            char *for_sql = "INSERT INTO person (surname, name, middle_name, sex) VALUES (";
-            char sql[512];
+                printf("Введите пол: ");
+                scanf_result = scanf("%2[^'\n']", prsn.sex);
+                if(scanf_result != 1){
+                    printf("Error: wrong input.\n");
+                    return 1;
+                }
+                getchar();
 
-            sprintf(sql, "%s'%s', '%s', '%s', '%s');", for_sql, prsn.surname, prsn.name, prsn.middle_name, prsn.sex);
+                printf("---------------------------------------------------\n \
+                    \rВнимание! Проверьте введенные данные выше!\n \
+                    \r--------------------------------------------------- \n \
+                    \r           1. Верно, продолжить\n \
+                    \r           2. Неверно, вернуться в меню\n \
+                    \r---------------------------------------------------\n\n"); 
+                
+                int select_menu_after_input;
+
+                while((select_menu_after_input = getchar()) != EOF) {
+                    switch(select_menu_after_input){
+                        case '1':
+                            getchar();
+                            printf("Выбарно: Данные введены верно. ");
+                            break;
+                        
+                        case '2':
+                            getchar();
+                            printf("Выбарно: Данные введены неверно. Запись в базу не проводилась.\n");
+                            break;
+
+                        case '\n':
+                            getchar();
+                            printf("удален лишний символ новой строки\n");
+                            continue;
+
+                        default:
+                            getchar();
+                            printf("Неверно введен символ при выборе, попробуйте снова\n");
+                            continue;
+                    }
+                    break;
+                }
+
+                if(select_menu_after_input == '2'){
+                    puts(MENU);
+                    continue;
+                }
+
+                char *for_sql = "INSERT INTO person (surname, name, middle_name, sex) VALUES (";
+                char sql[512];
+
+                sprintf(sql, "%s'%s', '%s', '%s', '%s');", 
+                    for_sql, prsn.surname, prsn.name, prsn.middle_name, prsn.sex);
 #ifdef DEBUG
-            printf("DEBUG: %s\n", sql);
+                printf("DEBUG: %s\n", sql);
 #endif
-            result = sqlite3_exec(db, sql, 0, 0, &err_msg);
+                result = sqlite3_exec(db, sql, 0, 0, &err_msg);
 
-            if(result != SQLITE_OK){
-                printf("SQL error: %s\n", err_msg);
-                sqlite3_free(err_msg);
-                sqlite3_close(db);
-                return 1;
+                if(result != SQLITE_OK){
+                    printf("SQL error: %s\n", err_msg);
+                    sqlite3_free(err_msg);
+                    sqlite3_close(db);
+                    return 1;
+                }
+                printf("и записаны в базу.\n");
+                break;
+
+            /* Search person */
+            case '2':
+                getchar();
+                printf("Вы выбрали Поиск лица!\nВыберите поиск по:\n \
+                    \r1. Фамилии\n \
+                    \r2. Имени\n \
+                    \r3. Отчеству\n");
+                
+                int select_menu_for_search;
+                char surname_for_search[MAX_NAME_LEN];
+                
+                while((select_menu_for_search = getchar()) != EOF) {
+                    switch(select_menu_for_search){
+                        /* Search surname */
+                        case '1':
+                            getchar();
+                            printf("Выбарно: Поиск по фамилии.\nВведите фамилию: ");
+
+                            scanf_result = scanf("%63[^'\n']", surname_for_search);
+                            if(scanf_result != 1){
+                                printf("Error: wrong input.\n");
+                                return 1;
+                            }
+                            getchar();
+
+                            char *for_sql = "SELECT * FROM person WHERE surname LIKE";
+                            char sql[512];
+
+                            sprintf(sql, "%s '%s';", for_sql, surname_for_search);
+
+                            result = sqlite3_exec(db, sql, callback, 0, &err_msg);
+                            if (result != SQLITE_OK ){
+                                printf("SQL error: %s\n", err_msg);
+                                sqlite3_free(err_msg);
+                                sqlite3_close(db);
+                                return 1;
+                            }
+#ifdef DEBUG
+                            printf("DEBUG: %s\n", sql);
+#endif                      
+                            break;
+                        
+                        case '2':
+                            getchar();
+                            printf("Выбарно: Поиск по имени\n");
+                            break;
+
+                        case '3':
+                            getchar();
+                            printf("Выбарно: Поиск по отчеству\n");
+                            break;
+
+                        case '\n':
+                            getchar();
+                            printf("Удален лишний символ новой строки\n");
+                            continue;
+
+                        default:
+                            getchar();
+                            printf("Неверно введен символ при выборе, попробуйте снова\n");
+                            continue;
+                    }
+
+                    break;
+                }
+            break;
+            
+            /* Edit person */    
+            case '3':
+                getchar();
+                printf("Вы выбрали редактирование лица!\n");
+                break;
+            
+            default:
+                getchar();
+                printf("Введен неверный символ для меню, выберит между 1-6\n");
+                continue;
             }
-
-            break;
-
-        case '2':
-            getchar();
-            printf("Вы выбрали Поиск лица!\n");
-            break;
-
-        case '3':
-            getchar();
-            printf("Вы выбрали редактирование лица!\n");
-            break;
-        
-        default:
-            getchar();
-            printf("Введен неверный символ для меню, выберит между 1-6\n");
-            break;
-        }
-        
+            
         puts(MENU);
     }
 
-    sqlite3_close(db); /* Закрываем подключение к БД */
+    /* Close database */
+    sqlite3_close(db);
     return 0;
 }
 
-// добавить к новым функциям static
+/* For search person */
+int callback(void *notUsed, int colCount, char **columns, char **colNames)
+{
+    for (int i = 0; i < colCount; i++)
+    {
+        printf("%s = %s\n", colNames[i], columns[i] ? columns[i] : "NULL");
+    }
+    printf("\n");
+    return 0;
+}
